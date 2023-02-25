a2cc0158565ad2518d432bd5cf93167f7ead8c5e
Mody-Medhat <moody.medhat99@gmail.com>
2023-02-25 23:10:52 +0200

    first commit

---
+++ run.py
@@ -0,0 +1,105 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
+import joblib
+import Preprocess
+
+# Load The Model
+XGB_model = joblib.load('XGB.pkl')
+
+# Read The Data
+car = pd.read_csv('Hatla2ee-Egypt-Used-Car-V5.CSV')
+
+# add new columns
+car['Brand'] = car['Brand'].str.replace(' ', '-')
+car["Brand-Model"] = car['Brand'].astype(str) + " " + car["Model"]
+car["Brand-Fule"] = car['Brand'].astype(str) + " " + car["Fule"].astype(str)
+car["Brand-Body"] = car['Brand-Model'].astype(str) + " " + car["Body"].astype(str)
+
+# Intialize the Flask APP
+app = Flask(__name__)
+cors = CORS(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    brands = sorted(car['Brand'].unique())
+    models = sorted(car['Brand-Model'].unique())
+    #city = sorted(car['City'].unique())
+    colors = sorted(car['Color'].unique())
+
+    vehicle_styles = sorted(car['Brand-Body'].unique())
+    car_styles = sorted(car['Body'].unique())
+    fule_types = sorted(car['Brand-Fule'].unique())
+    Engine_Fule_Types = sorted(car['Fule'].astype(str).unique())
+    
+    brands.insert(0, 'Select Brand...')
+    return render_template('index.html', 
+                           brands=brands,
+                           models=models,
+                           #city = city,
+                           colors = colors,
+                           vehicle_styles=vehicle_styles,
+                           fule_types=fule_types,
+                           Engine_Fule_Types=Engine_Fule_Types,
+                           car_styles=car_styles,)
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+@cross_origin()
+def predict():
+    brand =  request.form['brand']
+    brand  = brand.replace('-', ' ')
+    try:    
+        model =  request.form['model']
+        model = model.split(' ', 1)[1]
+        
+        body =  request.form['vehicle_style']
+        transmission =  request.form['Transmission']
+        fule =  request.form['fule']
+    except KeyError:
+        meg = 'There Is An Empty Field Please Recheck The Data!!'
+        return meg 
+    try:
+        year = int(request.form['year'])
+        engine_cc =  int(request.form['engine_cc'])
+        kilometers_Driven =  float(request.form['kilometers_Driven'])
+    except ValueError:
+        meg = 'There Is An Empty Field Please Recheck The Data!!'
+        return meg    
+    color =  request.form['color']
+    #city =  request.form['city']
+
+    data = {'Brand': brand,
+                         'Model': model,
+                         'Body': body,
+                         'Transmission': transmission,
+                         'Year': year,
+                         'Fule': fule,
+                         'Engine_CC': engine_cc,
+                         'Kilometers_Driven': kilometers_Driven,
+                         'Color': color,
+                         }
+    
+    final_data = Preprocess.preprossecing(data)
+    print('#'*200)
+    print (final_data)
+    print('#'*100)
+
+    prediction = XGB_model.predict(final_data)
+    #price = np.expm1(prediction)*1000 
+    prediction_price = '{:.0f}'.format(prediction[0])
+    #prediction_price = prediction[0]
+    #prediction_price = prediction_price.astype(int)
+
+    print(prediction_price) 
+
+    return prediction_price + ' EGP'
+
+# Run the App from the Terminal
+if __name__ == '__main__':
+    # app.run(debug=True,host='192.168.1.111',port=5000)
+    app.run(port=8080, debug=True)
+
+
---
+++ static/js/main.js
@@ -0,0 +1,157 @@
+
+function change_color(labl, checkboxName, labelname) {
+
+    clr = document.getElementsByName(checkboxName);
+    labls2 = document.getElementsByName(labelname);
+
+
+    for (var i = 0; i < clr.length; i++) {
+
+        if (clr[i].type === 'radio' && clr[i].checked) {
+            labl.style.backgroundColor = "#2091f1";
+            clr[i].style.color = "#F77D0A"
+        } else {
+            for (var e = 0; e < labls2.length; e++) {
+                if (labls2[e] != labl) {
+                    labls2[e].style.backgroundColor = "";
+                }
+            }
+            clr[i].style.color = ""
+        }
+
+    }
+}
+
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
+
+$(document).ready(function () {
+    // Initialize select2
+    $("#brand").select2();
+    $("#model").select2();
+    $("#color").select2();
+
+
+});
+
+$(document).ready(function () {
+    $("#year").datepicker({
+        format: "yyyy",
+        viewMode: "years",
+        minViewMode: "years",
+        autoclose: true
+    });
+})
+
+const ColorCode = {
+    Blue: "#007bff",
+    Gray: "#6c757d",
+    White: "#ffffff",
+    Mocha: "#6f372d",
+    Black: "#000000",
+    Gold: "#f9d77e",
+    Green: "#00FF00",
+    Silver: "#bebdb6",
+    'Dark red': "#8B0000",
+    Brown: "#35281e",
+    Bronze: "#51412d",
+    Beige: "#F5F5DC",
+    Champagne: "#eed9b6",
+    'Dark green': "#006400",
+    'Light grey': "#90ee90",
+    Beige: "#d9b99b",
+   'Dark blue': "#00008b",
+    Green: "#245336",
+    Eggplant: "#483248",
+    Cyan: "#00FFFF",
+    Yellow: "#ffc107",
+    Petroleum: "#005f6a",
+    Purple: "#800080",
+    Olive: "#808000",
+    Orange: "#FFA500",
+};
+
+
+function PrintData() {
+    var brand = document.getElementById("brand").value;
+
+    var model = document.getElementById("model").value;
+    console.log(model);
+    var year = document.getElementById("year").value;
+    var kilometers_Driven = document.getElementById("kilometers_Driven").value;
+    var color = document.getElementById("color").value;
+
+    var vehicle_style = document.getElementById("vehicle_style").value;
+
+    BrandModel = brand + " " + model;
+    // document.getElementById('brandData').innerHTML = BrandModel;
+    console.log(brand + " " + model)
+    document.getElementById('brandData').innerHTML = model;
+    document.getElementById('yearData').innerHTML = year;
+    document.getElementById('kilometers_DrivenData').innerHTML = kilometers_Driven;
+
+    document.getElementById('vehicle_styleData').innerHTML = vehicle_style;
+    for (const [key, value] of Object.entries(ColorCode)) {
+        if (key == color) {
+            document.getElementById("colorData").style.color = value;
+        }
+    }
+    // document.getElementById('colorData').innerHTML = color;
+}
+
+function required() {
+
+    // data send from form to card
+    const year = document.getElementById('year');
+    const engine_cc = document.getElementById('engine_cc');
+    const kilometers_Driven = document.getElementById('kilometers_Driven');
+    const fule = document.getElementById('fule');
+    //  const city_mpg = document.getElementById('city_mpg');
+
+
+    if (year.value === "") {
+        document.getElementById("year_label").style.color = "rgb(255 0 57)";
+        document.getElementById("year_label").style.fontWeight = "700";
+        year.style.border = "3px solid #ffc107"; 
+        y = 0;
+    }
+    else {
+        y = 1;
+        year.style.color = null;
+    }
+    if (engine_cc.value === "") {
+        document.getElementById("engine_cc_label").style.color = "rgb(255 0 57)";
+        engine_cc.style.border = "3px solid #ffc107"; 
+        hp = 0;
+    }
+    else {
+        hp = 1;
+    }
+    if (kilometers_Driven.value === "") {
+        document.getElementById("kilometers_Driven_label").style.color = "rgb(255 0 57)";
+        kilometers_Driven.style.border = "3px solid #ffc107"; 
+        cy = 0;
+    }
+    else {
+        cy = 1;
+    }
+    if (fule.value === "") {
+        document.getElementById("fule_label").style.color = "rgb(255 0 57)";
+        fule.style.border = "3px solid #ffc107"; 
+        h = 0;
+    }
+    else {
+        h = 1;
+    }
+    //return condition//
+    if (y == 1 && hp == 1 && cy == 1 && h == 1) {
+        return true;
+    }
+    else
+        return false;
+}
+function valdiateyear(){
+    
+}
